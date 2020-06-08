a8b8c5d13492907bb8724e4e29fe3043ba86265f
Liangyue Li <jackiey99@gmail.com>
2020-06-07 21:39:29 -0700

    dp

---
+++ leetcode/518_coin_change_2.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        dp = [[0 for _ in range(amount + 1)] for _ in range(n + 1)]
+        dp[0][0] = 1
+        for i in range(1, n + 1):
+            dp[i][0] = 1
+            for j in range(1, amount + 1):
+                dp[i][j] = dp[i - 1][j]
+                if j >= coins[i - 1]:
+                    dp[i][j] += dp[i][j - coins[i - 1]]
+
+        return dp[n][amount]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    amount = 5
+    coins = [1, 2, 5]
+    print(s.change(amount, coins))
