eb0a2a3d954efa1ee69e88e43d0bd230cecf45a5
Liangyue Li <jackiey99@gmail.com>
2020-06-19 09:05:58 -0700

    put numbers to their respective positions

---
+++ leetcode/41_first_missing_positive.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[i] != i + 1 and nums[i] != nums[nums[i] - 1]:
+                j = nums[i] - 1
+                nums[i], nums[j] = nums[j], nums[i]
+
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        return n + 1
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [3, 4, -1, 1]
+    print(s.firstMissingPositive(nums))
