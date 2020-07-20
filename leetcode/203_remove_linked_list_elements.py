3106de133970889cbaa36c6d7dade49211b324c2
Liangyue Li <jackiey99@gmail.com>
2020-07-20 10:51:06 -0700

    linked list

---
+++ leetcode/203_remove_linked_list_elements.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        prev, curr = dummy, head
+
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+
+            curr = curr.next
+        return dummy.next
