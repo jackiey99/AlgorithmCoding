a9dae83f96ff32a9426be65e3e7a88a4dfee70e2
Liangyue Li <jackiey99@gmail.com>
2020-09-10 17:29:23 -0700

    simple math

---
+++ codeforces/1335A_candies_and_two_sisters.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+#define ar array
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << (n-1)/2 << endl;
+    }
+    return 0;
+}
