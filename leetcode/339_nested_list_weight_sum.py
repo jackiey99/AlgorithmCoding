2c8c12a3726fd7a64386f171409e8ed50e8c9c77
Liangyue Li <jackiey99@gmail.com>
2020-10-18 21:41:15 -0700

    depth first search

---
+++ leetcode/339_nested_list_weight_sum.py
@@ -0,0 +1,57 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        res = 0
+        for ni in nestedList:
+            res += self.dfs(ni, 1)
+        return res
+
+    def dfs(self, ni, depth):
+        if ni.isInteger():
+            return depth * ni.getInteger()
+        res = 0
+        for nii in ni.getList():
+            res += self.dfs(nii, depth + 1)
+        return res
