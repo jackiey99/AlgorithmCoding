ac63a3ee1c279232918a99fde1fa6aa667ece8e5
Liangyue Li <jackiey99@gmail.com>
2020-10-21 11:37:03 -0700

    greey algorithm

---
+++ leetcode/12_integer_to_roman.py
@@ -0,0 +1,16 @@
+digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"),
+          (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman_numerals = []
+        for value, symbol in digits:
+            if num == 0:
+                break
+            if value > num:
+                continue
+            count = num // value
+            num = num % value
+            roman_numerals.append(count * symbol)
+        return ''.join(roman_numerals)
